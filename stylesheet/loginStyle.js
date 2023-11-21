4022aa6bb9d74097d8987bf05573005996c3b29b
Muhammed-Sirajudeen <muhammedsirajudeen29@gmail.com>
2023-11-22 00:29:26 +0530

    containerised the backend and database

--- stylesheet/loginStyle.js
+++ stylesheet/loginStyle.js
@@ -63,6 +63,7 @@ const loginStyle=StyleSheet.create(
             width:"100%",
             height:40,
             backgroundColor:Backgroundcolor,
+            borderRadius:10
         },
         logintext:{
             color:"white"
